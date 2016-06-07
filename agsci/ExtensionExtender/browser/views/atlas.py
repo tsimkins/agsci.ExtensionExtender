1abe426aebf5cba8f21c504bf1bfdff3c093b0f0
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-06-07 10:24:47 -0400

    Added a summary report for team content managers.

--- agsci/ExtensionExtender/browser/views/atlas.py
+++ agsci/ExtensionExtender/browser/views/atlas.py
@@ -19,10 +19,10 @@ except ImportError:
 class AtlasContentReview(FolderView):
 
     review_state = ["atlas-pending", ]
-    
+
     app_title = "Atlas Content Review"
 
-    sections = ['animals', 'plants', 'pests', 'food', 'health', 
+    sections = ['animals', 'plants', 'pests', 'food', 'health',
                 'natural-resources', 'youth', 'community', 'business']
 
     content_types = ['PhotoFolder', 'Food', 'Link', 'File', 'Folder', 'Document',]
@@ -38,23 +38,23 @@ class AtlasContentReview(FolderView):
     }
 
     default_nav = [
-                    'atlas_owner_review', 'atlas_feedback_review', 
+                    'atlas_owner_review', 'atlas_feedback_review',
                     'atlas_ready_review', 'atlas_archive_review'
                   ]
 
     nav_items_by_role = {
         'Atlas Manager' : [
-                            'atlas_owner_review', 'atlas_pre_review', 
-                            'atlas_invalid_owner', 'atlas_web_review', 
+                            'atlas_owner_review', 'atlas_pre_review',
+                            'atlas_invalid_owner', 'atlas_web_review',
                             'atlas_feedback_review', 'atlas_ready_review',
                             'atlas_archive_review'
                         ],
         'Atlas Content Manager' : [
-                            'atlas_owner_review',  
-                            'atlas_invalid_owner',  
-                            'atlas_feedback_review', 
+                            'atlas_owner_review',
+                            'atlas_invalid_owner',
+                            'atlas_feedback_review',
                             'atlas_ready_review',
-                            'atlas_archive_review' 
+                            'atlas_archive_review'
                         ],
     }
 
@@ -83,17 +83,17 @@ class AtlasContentReview(FolderView):
 
     def needsReview(self, brain=None):
         if brain:
-        
+
             if brain.portal_type in ['Subsite', 'Section', 'Blog']:
                 return False
-            
+
             review_state_match = (brain.review_state in self.review_state)
-            
+
             user_id = self.getUserId()
-            
+
             if user_id:
                 return (brain.Creator == user_id) and review_state_match
-            
+
             return review_state_match
 
         return False
@@ -102,13 +102,13 @@ class AtlasContentReview(FolderView):
         if brain:
             if len(self.review_state) > 1:
                 return 'state-%s' % brain.review_state
-            
+
         return ''
-        
+
     def getItemURL(self, brain=None):
         if brain:
             return brain.getURL()
-            
+
         return None
 
     def getNavigationItemData(self, view_name):
@@ -119,12 +119,12 @@ class AtlasContentReview(FolderView):
 
     def getNavigationItemsByRole(self):
         role = self.getAtlasRole()
-        
+
         return self.nav_items_by_role.get(role, self.default_nav)
 
     def navigation_items(self):
 
-        user_id = getattr(self, 'user_id', None) 
+        user_id = getattr(self, 'user_id', None)
 
         return [self.getNavigationItemData(x) for x in self.getNavigationItemsByRole()]
 
@@ -156,31 +156,31 @@ class AtlasContentReview(FolderView):
     def getUserId(self):
 
         user_id = getattr(self, 'user_id', self.request.form.get('user_id', None))
-        
+
         if user_id:
 
             if user_id == 'all':
                 return None
 
             return user_id
-        
+
         if not self.isPowerUser():
             user = self.portal_membership.getAuthenticatedMember()
-            
+
             if user:
                 return user.getId()
-            
+
         return None
 
 
     @memoize
     def getAtlasRole(self):
         atlas_roles = ['Atlas Manager', 'Atlas Content Manager']
-    
+
         user = self.portal_membership.getAuthenticatedMember()
-        
+
         user_roles = filter(lambda x:atlas_roles.count(x), user.getRolesInContext(self.context))
-        
+
         try:
             return user_roles[0]
         except IndexError:
@@ -190,17 +190,17 @@ class AtlasContentReview(FolderView):
     def showOwnerFilter(self):
         if self.getUserId():
             return False
-        
+
         if not self.getReviewQueueOwners():
             return False
-    
+
         return self.isPowerUser()
 
 
     def isPowerUser(self):
         return (self.isAtlasManager() or self.isAtlasContentManager())
 
-        
+
     def isAtlasManager(self):
         return (self.getAtlasRole() == 'Atlas Manager')
 
@@ -210,13 +210,13 @@ class AtlasContentReview(FolderView):
 
     @memoize
     def getReviewStructure(self):
-    
+
         site_url = '/'.join(getSite().getPhysicalPath())
-    
+
         paths = []
-    
+
         pending = self.getReviewQueue()
-        
+
         for r in pending:
             r_path = r.getPath().split('/')
             for i in range(len(site_url.split('/'))+1,len(r_path)+1):
@@ -228,24 +228,24 @@ class AtlasContentReview(FolderView):
 
         for r in results:
             bh.add(r)
-        
+
         return bh.getStructure(bh.getRootPaths())
 
 
     def getSectionPaths(self):
 
-        # If we're called at the root of the Plone site        
+        # If we're called at the root of the Plone site
         if self.isPloneSite:
 
             site = getSite()
-            
+
             # Find main section ids that are in the sections list
             section_ids = list(set(site.objectIds()) & set(self.sections))
 
             # If we found them, just use those sections
             if section_ids:
                 return ['/'.join(site[x].getPhysicalPath()) for x in section_ids]
-        
+
         # Otherwise, just return the path of the current context
         return '/'.join(self.context.getPhysicalPath())
 
@@ -263,7 +263,7 @@ class AtlasContentReview(FolderView):
             query['path'] = paths
 
         user_id = self.getUserId()
-        
+
         if user_id:
             query['Creator'] = user_id
 
@@ -271,52 +271,72 @@ class AtlasContentReview(FolderView):
 
         return self.portal_catalog.searchResults(query)
 
-    @memoize
-    def getValidIds(self):
-    
-        valid_people = self.portal_catalog.searchResults({'portal_type' : 'FSDPerson', 'expires' : {'query' : DateTime(), 'range' : 'min'}})
 
-        return [x.getId for x in valid_people]
+    @memoize
+    def getValidPeople(self):
+
+        return self.portal_catalog.searchResults({'portal_type' : 'FSDPerson', 'expires' : {'query' : DateTime(), 'range' : 'min'}})
+
+
+    @memoize
+    def getValidPeopleIds(self):
+
+        return map(lambda x: x.getId, self.getValidPeople())
+
+
+    @memoize
+    def getValidPeopleData(self):
+
+        return dict(map(lambda x: (x.getId, x.Title), self.getValidPeople()))
+
+
+    @memoize
+    def getPersonNameById(self, person_id=None):
+
+        return self.getValidPeopleData().get(person_id, 'Invalid User')
+
 
     @memoize
     def getInvalidIds(self):
 
-        valid_people_ids = self.getValidIds()
-        
+        valid_people_ids = self.getValidPeopleIds()
+
         creators = self.portal_catalog.uniqueValuesFor('Creator')
-        
+
         return list(set(creators) - set(valid_people_ids))
+
 
     def getReviewQueueOwners(self):
         return set([x.Creator for x in self.getReviewQueue()])
 
 
 class AtlasFeedbackReview(AtlasContentReview):
-    
+
     review_state = ["atlas-feedback", ]
-    
+
 
 class AtlasWebReview(AtlasContentReview):
-    
+
     review_state = ["atlas-web-team-review", ]
 
 
 class AtlasPreReview(AtlasWebReview):
 
     review_state = ["published", ]
-    
+
     show_actions = True
 
 
 class AtlasReadyReview(AtlasWebReview):
-    
+
     review_state = ["atlas-ready", ]
 
+
 class AtlasArchiveReview(AtlasWebReview):
-    
+
     review_state = ["atlas-archive", ]
-    
-    
+
+
 class AtlasInvalidOwner(AtlasWebReview):
 
     review_state = ['published', 'atlas-pending', 'atlas-web-team-review', 'atlas-feedback', 'atlas-ready']
@@ -328,11 +348,11 @@ class AtlasInvalidOwner(AtlasWebReview):
             url = super(AtlasInvalidOwner, self).getItemURL(brain=brain)
             url = addTokenToUrl('%s/edit' % url)
             return '%s#fieldsetlegend-ownership' % url
-            
+
         return None
 
     def getWorkflowActions(self, brain=None):
-    
+
         if brain and hasattr(brain, 'getObject'):
 
             # Only show Atlas Archive action for invalid owner view
@@ -342,13 +362,13 @@ class AtlasInvalidOwner(AtlasWebReview):
         return []
 
     def needsReview(self, brain=None):
-    
+
         if brain:
 
             # If we otherwise need to review this, also check to make sure that the owner is invalid.
             if super(AtlasInvalidOwner, self).needsReview(brain=brain):
-                return (brain.Creator not in self.getValidIds())
-            
+                return (brain.Creator not in self.getValidPeopleIds())
+
         return False
 
     @memoize
@@ -368,10 +388,10 @@ class AtlasInvalidOwner(AtlasWebReview):
         user_id = self.getUserId()
 
         if user_id:
-            query['Creator'] = user_id        
+            query['Creator'] = user_id
         elif invalid_ids:
             query['Creator'] = invalid_ids
-            
+
         query['portal_type'] = self.content_types
 
         return self.portal_catalog.searchResults(query)
@@ -381,54 +401,133 @@ class BrainHierarchy(object):
     def __init__(self, site_url):
         self.site_url=site_url
         self.items = {}
-        self.children = {}    
+        self.children = {}
 
     def site_url(self):
         return '/'.join(self.site.getPhysicalPath())
-    
+
     def add(self, brain):
         # Get path of self and parent
         brain_path = brain.getPath()[len(self.site_url)+1:]
         brain_parent_path = '/'.join(brain_path.split('/')[0:-1])
-        
+
         # Add brain_path to brain
         self.items[brain_path] = brain
 
         # Create an entry for self children
         if not self.children.has_key(brain_path):
             self.children[brain_path] = []
-        
+
         # Create/update an entry for self parent children
         if not self.children.has_key(brain_parent_path):
             self.children[brain_parent_path] = []
-            
+
         self.children[brain_parent_path].append(brain_path)
-        
+
         return True
-    
+
     def getRootPaths(self):
         return [x for x in self.items.keys() if '/' not in x]
-    
+
     def getStructure(self, paths=[]):
 
         data = []
-            
+
         for p in paths:
             data.append({'path' : p, 'brain' : self.items.get(p), 'children' : self.getStructure(self.children[p])})
-        
+
         return data
-        
+
 class AtlasGenericRedirect(AtlasContentReview):
 
     def __call__(self):
         context = self.context
-        
+
         if self.isDefaultPage():
             context = self.context.aq_parent
-    
+
         url = '%s/@@atlas_owner_review' % context.absolute_url()
 
         if self.isAtlasManager():
             url = '%s/@@atlas_web_review' % context.absolute_url()
 
         return self.context.REQUEST.RESPONSE.redirect(url)
+
+class AtlasStatusSummary(AtlasContentReview):
+
+    review_state_data = {
+        'atlas-archive' : 'atlas_archive_review',
+        'atlas-feedback' : 'atlas_feedback_review',
+        'atlas-import-article' : 'atlas_imported_review', # Does not exist
+        'atlas-pending' : 'atlas_owner_review',
+        'atlas-ready' : 'atlas_ready_review',
+        'atlas-web-team-review' : 'atlas_web_review',
+    }
+
+    review_state = review_state_data.keys()
+
+    owner_waiting_review_states = ['atlas-feedback', 'atlas-pending']
+
+    @memoize
+    def getResults(self):
+
+        query = {
+            'review_state' : self.review_state,
+        }
+
+        # Restrict query to content in team portions of site
+        paths = self.getSectionPaths()
+
+        if paths:
+            query['path'] = paths
+
+        query['portal_type'] = self.content_types
+
+        return self.portal_catalog.searchResults(query)
+
+
+    def getReviewStateReport(self):
+
+        data = {}
+
+        for r in self.getResults():
+            review_state = r.review_state
+            view_id = self.review_state_data.get(review_state, 'N/A')
+
+            if not data.has_key(view_id):
+                data[view_id] = []
+
+            data[view_id].append(r)
+
+        return data
+
+
+    def getReviewStateByOwnerReport(self):
+
+        data = {}
+
+        for r in self.getResults():
+            review_state = r.review_state
+            view_id = self.review_state_data.get(review_state, 'N/A')
+
+            if review_state in self.owner_waiting_review_states:
+
+                creator = r.Creator
+
+                if creator not in self.getValidPeopleIds():
+                    creator = 'invalid_user'
+
+                if not data.has_key(view_id):
+                    data[view_id] = {}
+
+                if not data[view_id].has_key(creator):
+                    data[view_id][creator] = []
+
+                data[view_id][creator].append(r)
+
+        return data
+
+
+    @property
+    def title(self):
+        return '%s: %s' % (self.app_title, 'Summary')
