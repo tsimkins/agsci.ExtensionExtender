441cfdcb5b6b84ffe3d8eab49c411d901dafe261
TIMOTHY RIDGE SIMKINS <trs22@psu.edu>
2016-09-09 09:43:40 -0400

    Made Atlas Import workflow recursively update workflow state on child objects without calling the import API on them.

--- agsci/ExtensionExtender/events/atlas.py
+++ agsci/ExtensionExtender/events/atlas.py
@@ -1,4 +1,6 @@
 import requests
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.WorkflowCore import WorkflowException
 
 # Call to external Plone system when content is set to import workflow state
 
@@ -13,7 +15,65 @@ workflow_to_import_url = {
 
 def importContent(context, event):
 
-    # Look up import URL based on event action
+    # Verify that the workflow event action is one for which we're going to
+    # import the content
+    if event.action not in workflow_to_import_url.keys():
+        return False
+
+    # Get the parent object
+    parent = context.aq_parent
+
+    # Get the portal_workflow tool, and find the review states for this object
+    # and the parent object
+    wftool = getToolByName(context, 'portal_workflow')
+
+    # Check if the parent's workflow state indicates that it's imported already.
+    # If it is, don't import this object.  If the parent has no workflow state,
+    # this probably means it's a Plone site.
+    try:
+        parent_review_state = wftool.getInfoFor(parent, 'review_state')
+    except WorkflowException:
+        parent_review_state = None
+
+    if parent_review_state in ['atlas-import-article', 'atlas-import-video']:
+        return False
+
+    # This pushes the content from the current site to the new site via the API
+    callImportAPI(context, event)
+
+    # If the import was successful, set the status of all child objects that
+    # are in the Atlas Ready state to Imported
+
+    # Find all Atlas Ready items inside this object
+    portal_catalog = getToolByName(context, "portal_catalog")
+
+    context_path = "/".join(context.getPhysicalPath())
+
+    results = portal_catalog.searchResults({'review_state' : 'atlas-ready',
+                                            'path' : context_path
+                                            })
+
+    # For those items, call the workflow action specified by the parent event
+    for r in results:
+
+        # Skip current object
+        if r.UID == context.UID():
+            continue
+
+        o = r.getObject()
+
+        try:
+            wftool.doActionFor(o, event.action)
+        except WorkflowException:
+            continue
+        else:
+            o.reindexObject()
+            o.reindexObjectSecurity()
+
+def callImportAPI(context, event):
+
+    # Look up import URL based on event action.  If the URL doesn't exist in
+    # the dict, there's no import done on it.
     import_url = workflow_to_import_url.get(event.action, None)
 
     # Return if we didn't find one
@@ -27,17 +87,17 @@ def importContent(context, event):
     post_data = {'UID' : uid}
     response = requests.post(import_url, data=post_data)
 
-    # Response, status etc
-    response.text
-    response.status_code
-
+    # If we received a 200 (success) return
     if response.status_code == 200:
+
+        # Grab the JSON data
         data = response.json()
 
-        if data.get('external_url', None):
-            return True
+        # If the import returned a 200, but doesn't have a value for the
+        # 'external_url' key, raise an exception. This is a basic sanit check.
+        if not data.get('external_url', None):
+            raise Exception('Return from import indicates issue: %s' % repr(json))
 
+    # Raise an exception if the return code is not 200
     else:
         raise Exception('%d: %s' % (response.status_code, response.text))
-
-    return False
